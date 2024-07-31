a95bbe45f20d72931dea98aadf56b65e60f4ea9c
Krich2022 <117894211+Krich2022@users.noreply.github.com>
2024-07-30 22:39:17 -0600

    Boilerplate Code

---
+++ src/main.jsx
@@ -0,0 +1,30 @@
+import ReactDOM from "react-dom/client";
+// Bringing in the required imports from 'react-router-dom' to set up application routing behavior
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+
+// Bringing in the pages the router will use to conditionally show the appropriate views
+import App from "./App";
+import ErrorPage from "./pages/error/Error";
+
+// Define the accessible routes, and which components respond to which URL
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <Signin />,
+      },
+    ],
+  },
+]);
+
+// Render the RouterProvider component
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
